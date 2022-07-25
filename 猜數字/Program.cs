60040e4bad6bbf0514d5516e23896e95eaf856e3
韋賢 <shesid2698@gmail.com>
2022-07-25 22:42:15 +0800

    init

---
+++ 猜數字/Program.cs
@@ -0,0 +1,33 @@
+﻿//猜數字
+int secret_num=116;
+int guess;
+int guess_count = 0;
+bool win=false;
+
+
+do
+{
+    Console.Write("請輸入猜測數字:");
+    guess = int.Parse(Console.ReadLine());
+    guess_count++;
+
+    if (guess > 116)
+    {
+        System.Console.WriteLine("請猜小一點");
+    }
+    else if (guess < 116)
+    {
+        System.Console.WriteLine("請猜大一點");
+    }
+    else
+    {
+        System.Console.WriteLine("恭喜答對");
+        win = true;
+    }
+}
+while (guess != secret_num && guess_count< 3 );
+
+if (!win)
+{
+    System.Console.WriteLine("你已經死了");
+}
---
+++ 猜數字改良版/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 猜數字改良版
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random secret = new Random();
+            int ans = secret.Next(-10,10);
+            bool win = true;
+            Console.WriteLine("====猜數字遊戲====");
+            int count = 1;
+            while (win)
+            {
+                
+                Console.Write("請輸入數字 : ");
+                int input = int.Parse(Console.ReadLine());
+ 
+                if (input < ans)
+                {
+                    Console.WriteLine("再大一點");
+                }
+                else if (input > ans)
+                {
+                    Console.WriteLine("再小一點");
+                }
+                else
+                {
+                    Console.WriteLine("答對了");
+                    win = false;
+                    break;
+                }
+
+                if (count == 3)
+                {
+                    Console.WriteLine("你輸了!!");
+                    win = false;
+                }
+                count++;
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
